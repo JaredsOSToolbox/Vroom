64032f7bd00ad8e2307fb46eb58f4a6cf98c0505
JaredDyreson <jared.dyreson@gmail.com>
2021-04-25 12:11:54 -0700

    page table is now templated

--- includes/page_table_t.hpp
+++ includes/page_table_t.hpp
@@ -11,37 +11,43 @@
 #define FREE_LIST_SIZE 128
 #define PAGE_TABLE_SIZE FREE_LIST_SIZE
 
-struct page_entry_t {
-  uint32_t address; // address used
+#define _T template<typename T>
+
+_T
+
+struct entry_t {
+  T data; // data used (in our case this will be addresses)
   uint8_t bit; // dirty bit
   int reference_count; // current reference count
   int maximum_referernces;  // set timer on the number of times an object can be
                             // referenced
-  page_entry_t() {}
-  page_entry_t(uint32_t _address) {
-    address = _address;
+  entry_t() {}
+  entry_t(T _data) {
+    data = _data;
     bit = 0; // set dirty bit to false
     maximum_referernces =  MAX_REF_COUNT;
   }
 };
 
-typedef struct page_entry_t page_entry_t;
+//typedef struct entry_t<T> entry_t<T>;
+
+_T
 
 class page_table_t {
   public:
     page_table_t() {
-      this->entries = std::vector<page_entry_t*>(PAGE_TABLE_SIZE);
+      this->entries = std::vector<struct entry_t<T>*>(PAGE_TABLE_SIZE);
       this->in_use = std::list<size_t>(FREE_LIST_SIZE);
     }
 
-    page_entry_t* operator[](size_t index) {
-      page_entry_t* obj = this->entries[index];
+    struct entry_t<T>* operator[](size_t index) {
+      struct entry_t<T>* obj = this->entries[index];
 
       if(obj->reference_count == 0 || obj->maximum_referernces == 1) {
         delete this->entries[index];
         for(auto it = this->in_use.begin(); it != this->in_use.end(); ++it) {
           if(*it == index) {
-            it = this->in_use.erase(it);
+            this->in_use.erase(it);
             break;
           }
         }
@@ -56,28 +62,28 @@ class page_table_t {
       return obj;
     }
 
-    void insert(page_entry_t* entry, int position) {
+    void insert(struct entry_t<T>* entry, int position) {
       this->entries.insert(this->entries.begin() + position, entry);
       this->in_use.push_front(position);
     }
 
   private:
-    std::vector<page_entry_t*> entries;
+    std::vector<struct entry_t<T>*> entries;
     std::list<size_t> in_use;
 }; 
 
 int main(void) {
-  page_table_t t = page_table_t();
+  page_table_t<uint32_t> t = page_table_t<uint32_t>();
 
-  page_entry_t* entity = new page_entry_t((uint32_t)100);
+  struct entry_t<uint32_t>* entity = new entry_t((uint32_t)100);
 
   t.insert(entity, 0);
 
   for(int i = 0; i < 107; ++i) {
-    page_entry_t* element = t[0];
+    entry_t<uint32_t>* element = t[0];
     if(element != nullptr) {
-      std::cout << element->reference_count  << std::endl;
-      std::cout  << "max reference count is " << element->maximum_referernces << std::endl;
+        std::cout << element->reference_count  << std::endl;
+        std::cout  << "max reference count is " << element->maximum_referernces << std::endl;
     } else {
       std::cout << "freeing from the free list" << std::endl;
       break;
@@ -86,4 +92,3 @@ int main(void) {
 
   return 0;
 }
-
